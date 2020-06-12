dc8b650bc477affeac3bd63d1302bd6b0ba62dc9
jasbirs-work <jasbirsinghmejas1@gmail.com>
2020-06-12 18:02:55 +0530

    pic path

--- src/app/social-icon.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SocialIconDirective } from './social-icon.directive';
-
-describe('SocialIconDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SocialIconDirective();
-    expect(directive).toBeTruthy();
-  });
-});
